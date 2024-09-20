bc66ce56a9fefb38323bb85ab1a9e2f479c93991
Danilo <danilogmoura@gmail.com>
2024-09-20 02:18:00 -0300

    7. Operadores Aritiméticos #02

---
+++ Inicial/Controlando Código/operadores/aritmeticos_2.js
@@ -0,0 +1,17 @@
+let a = 8
+let b = 11
+
+// usando variáveis
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a % 4)
+console.log(a % 5)
+
+let resultado = (a + b) * a - b
+console.log(resultado)
+
+a = a + b * a / b
+console.log(a)
