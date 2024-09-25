7aacd3c5f9385314693ce18ff8e3261d31f7d4a8
Danilo <danilogmoura@gmail.com>
2024-09-25 13:33:20 -0300

    7. Com uso de função #02

---
+++ Inicial/Evoluindo na Programação/com_uso_funcao_2.js
@@ -0,0 +1,19 @@
+function exibirData(dia, mes, ano) {
+    console.log(`Dia: ${dia}`)
+    console.log(`Mêz: ${mes}`)
+    console.log(`Ano: ${ano}`)
+    console.log(`${dia} de ${mes} de ${ano}`)
+}
+
+const d1 = 7
+const m1 = 'Maio'
+const a1 = 2022
+
+const d2 = 24
+const m2 = 'Dezembro'
+const a2 = 2024
+
+exibirData(22, 'Novembro', 2026)
+exibirData(d1, m1, a1)
+exibirData(d2, m2, a2)
+
