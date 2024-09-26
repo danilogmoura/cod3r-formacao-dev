857105bf931b10083d0a0f2fe93da6f0f4872092
Danilo <danilogmoura@gmail.com>
2024-09-26 17:37:25 -0300

    18. Armazenar Função em Variável #01

---
+++ Fundamentos/Inicial/Evoluindo na Programação/funcoes/armazenar_funcao_em_variavel_1.js
@@ -0,0 +1,14 @@
+// Function Declaration => Declaração de Função
+function somar(a, b) {
+    return a + b
+}
+
+// Function Expression
+const subtrair = function (a, b) {
+    return a - b
+}
+
+
+console.log(somar(30, 27))
+
+console.log(subtrair(30, 27))
