2f1c40ff8cbd8394751a67147cbc28656b49cf6a
Danilo <danilogmoura@gmail.com>
2024-09-26 17:44:00 -0300

    Create verificar_se_funcao.js

---
+++ Fundamentos/Inicial/Evoluindo na Programação/funcoes/verificar_se_funcao.js
@@ -0,0 +1,21 @@
+let numero = 3
+let texto = 'Bom Dia'
+
+
+function somar(n1, n2) {
+    return n1 + n2
+}
+
+
+const subtrair = function (n1, n2) {
+    return n1 - n2
+}
+
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof somar)
+console.log(typeof subtrair)
+
+
+console.log(typeof subtrair === 'function')
