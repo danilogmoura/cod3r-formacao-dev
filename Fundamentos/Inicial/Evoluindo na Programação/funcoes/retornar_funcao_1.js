1e6344abc94301f4bf234688f4001aea3aead556
Danilo <danilogmoura@gmail.com>
2024-10-01 15:37:23 -0300

    25. Função Retornando Função #01

---
+++ Fundamentos/Inicial/Evoluindo na Programação/funcoes/retornar_funcao_1.js
@@ -0,0 +1,14 @@
+function externa() {
+    function interna(){
+        return 'Estou na função interna!'
+    }
+
+    return interna
+}
+
+console.log(externa)
+console.log(externa())
+console.log(externa()())
+
+const funcaoInterna = externa()
+console.log(funcaoInterna())
