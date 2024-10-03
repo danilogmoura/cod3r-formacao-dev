2ca7849ae3831297c06332d4496ae89b393f61ef
Danilo <danilogmoura@gmail.com>
2024-10-03 14:48:50 -0300

    10. Arrow - #06

---
+++ Fundamentos/Inicial/Funções em JavaScript/fundamentos/arrow_06.js
@@ -0,0 +1,14 @@
+// somar(3)(4)(5)
+
+const somar = (a) => {
+    return (b) => {
+        return (c) => a + b + c
+    }
+}
+
+console.log(somar(1)(2)(3))
+
+
+const somar2 = (a) => (b) => (c) => a + b + c
+
+console.log(somar2(1)(2)(3))
