7a804443b54c386838c7541a8fcd6838b3a762d3
Danilo <danilogmoura@gmail.com>
2024-10-02 22:10:16 -0300

    57. Função Objeto #02

---
+++ Fundamentos/Inicial/Evoluindo na Programação/objetos/funcao_e_objeto_2.js
@@ -0,0 +1,18 @@
+function criarData(dia = 1, mes = 1, ano = 1970) {
+    return {
+        dia,
+        mes,
+        ano,
+        formatar() {
+            return `${this.dia}/${this.mes}/${this.ano}`
+        }
+    }
+}
+
+const data1 = criarData(7, 11, 2026)
+const data2 = criarData(3, 3, 2023)
+const data3 = criarData(24, 12, 2025)
+
+console.log(data1.formatar())
+console.log(data2.formatar())
+console.log(data3.formatar())
