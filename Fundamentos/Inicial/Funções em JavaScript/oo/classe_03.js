931bfbeedcf835b6f205102959962790b6e0c57f
Danilo <danilogmoura@gmail.com>
2024-10-08 01:59:09 -0300

    66. Classe #03 - Parte 3

--- Fundamentos/Inicial/Funções em JavaScript/oo/classe_03.js
+++ Fundamentos/Inicial/Funções em JavaScript/oo/classe_03.js
@@ -5,11 +5,11 @@ class Data {
 
     // Getters / Setters
 
-    getDia() {
+    get dia() {
         return this.#dia
     }
 
-    setDia(valor) {
+    set dia(valor) {
         if (valor >= 1 && valor <= 31) {
             this.#dia = valor
         } else {
@@ -17,14 +17,38 @@ class Data {
         }
     }
 
+    get mes() {
+        return this.#mes
+    }
+
+    set mes(mes) {
+        if (mes >= 1 && mes <= 12) {
+            this.#mes = mes
+        } else {
+            console.log('Mês inválido')
+        }
+    }
+
+    get ano() {
+        return this.#ano
+    }
+
+    set ano(ano) {
+        if (typeof ano === 'number' && ano >= 1970 && ano <= 2900) {
+            this.#ano = ano
+        } else {
+            console.log('Ano inválido')
+        }
+    }
+
     exibir() {
         return `${this.#dia}/${this.#mes}/${this.#ano}`
     }
-
 }
 
 const data1 = new Data()
 
-data1.setDia(30)
-console.log(data1.getDia())
+data1.dia = 13
+data1.ano = 2040
+console.log(data1.dia)
 console.log(data1.exibir())
